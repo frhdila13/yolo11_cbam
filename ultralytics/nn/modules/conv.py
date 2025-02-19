901359bd7124d863f9f4d67ff9ca6ad83a9e2128
frhdila13 <154692686+frhdila13@users.noreply.github.com>
2025-02-20 06:42:32 +0800

    Update conv.py
    Signed-off-by: frhdila13 <154692686+frhdila13@users.noreply.github.com>

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -343,30 +343,29 @@ class CoordAtt(nn.Module):
         self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
         self.pool_w = nn.AdaptiveAvgPool2d((1, None))
 
-        mip = max(8, inp // reduction)
+        mip = max(8, inp // reduction)  # Reduced intermediate channels
 
         self.conv1 = nn.Conv2d(inp, mip, kernel_size=1, stride=1, padding=0)
         self.bn1 = nn.BatchNorm2d(mip)
         self.act = h_swish()
-        
-        self.conv_h = nn.Conv2d(mip, oup, kernel_size=1, stride=1, padding=0)
-        self.conv_w = nn.Conv2d(mip, oup, kernel_size=1, stride=1, padding=0)
-        
+
+        self.conv_h = nn.Conv2d(mip, inp, kernel_size=1, stride=1, padding=0)  # oup = inp
+        self.conv_w = nn.Conv2d(mip, inp, kernel_size=1, stride=1, padding=0)  # oup = inp
 
     def forward(self, x):
         identity = x
         
-        n,c,h,w = x.size()
+        n, c, h, w = x.size()
         x_h = self.pool_h(x)
-        x_w = self.pool_w(x).permute(0, 1, 3, 2)
+        x_w = self.pool_w(x).permute(0, 1, 3, 2)  # Transpose width & height
 
         y = torch.cat([x_h, x_w], dim=2)
         y = self.conv1(y)
         y = self.bn1(y)
-        y = self.act(y) 
-        
+        y = self.act(y)
+
         x_h, x_w = torch.split(y, [h, w], dim=2)
-        x_w = x_w.permute(0, 1, 3, 2)
+        x_w = x_w.permute(0, 1, 3, 2)  # Transpose back
 
         a_h = self.conv_h(x_h).sigmoid()
         a_w = self.conv_w(x_w).sigmoid()
