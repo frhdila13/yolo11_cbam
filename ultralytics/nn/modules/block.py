a39c3ea17afe8236691e4995a19134431337aae3
frhdila13 <154692686+frhdila13@users.noreply.github.com>
2025-02-19 22:45:29 +0800

    Update block.py
    Signed-off-by: frhdila13 <154692686+frhdila13@users.noreply.github.com>

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -7,8 +7,8 @@ import torch.nn.functional as F
 
 from ultralytics.utils.torch_utils import fuse_conv_and_bn
 
-from .conv import Conv, DWConv, GhostConv, LightConv, RepConv, autopad
-from .transformer import TransformerBlock
+from .conv import Conv, DWConv, GhostConv, LightConv, RepConv, autopad, ECAAttention
+from .transformer import TransformerBlock  
 
 __all__ = (
     "DFL",
