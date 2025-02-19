310ec4041989a2a8b3023db131b03085d65ad0f6
frhdila13 <154692686+frhdila13@users.noreply.github.com>
2025-02-19 15:49:05 +0800

    Update conv.py
    Signed-off-by: frhdila13 <154692686+frhdila13@users.noreply.github.com>

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -23,6 +23,7 @@ __all__ = (
     "RepConv",
     "Index",
     "ECAAttention",
+    "SEAttention",
 )
 
 
@@ -298,6 +299,24 @@ class ECAAttention(nn.Module):
         # Multi-scale information fusion
         y = self.sigmoid(y)
 
+        return x * y.expand_as(x)
+
+
+class SELayer(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, w, h = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
         return x * y.expand_as(x)
 
 
