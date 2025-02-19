664398aab8fee256668d78634aaf4f078f16b63b
frhdila13 <154692686+frhdila13@users.noreply.github.com>
2025-02-19 13:54:00 +0800

    Update tasks.py
    Signed-off-by: frhdila13 <154692686+frhdila13@users.noreply.github.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -65,7 +65,7 @@ from ultralytics.nn.modules import (
     WorldDetect,
     v10Detect,
     TransformerBlock,
-    ECAAttention
+    ECAAttention,
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
